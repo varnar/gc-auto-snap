d865631f7e80e6246d22edf0a7ce5310e975c2a9
Vardan <varnar@users.noreply.github.com>
2018-06-18 21:48:04 -0700

    Update funcs.sh

--- inc/funcs.sh
+++ inc/funcs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 ###----------------------------------------------------------------------------
 ### Script Name:    funcs.sh
 ### Description:    Fuctions for main script
@@ -31,6 +31,7 @@ gcd_run_gcloud_snap ()
     DISK_NAME=$1
     host_zone=$2
     DAYS_RETENTION=$3
+    HOST_NAME=$4
     retention_date=$(date -d "+${DAYS_RETENTION} days" "+%Y%m%d")
     logMsg "INFO" "Creating ${SNAP_PREFIX}-${DISK_NAME:0:31} for $DISK_NAME in ${host_zone} retention date ${retention_date}"
     gcloud compute disks snapshot $DISK_NAME --snapshot-names ${SNAP_PREFIX}-${DISK_NAME:0:31} --zone ${host_zone} --quiet
@@ -40,6 +41,7 @@ gcd_run_gcloud_snap ()
     if [ $exit_code -eq 0 ]
     then
        gcloud compute snapshots add-labels ${SNAP_PREFIX}-${DISK_NAME:0:31} --labels='retention_date='${retention_date}''
+       echo ${SNAP_PREFIX}-${DISK_NAME:0:31} >> ${SCRIPT_DIR}/${HOST_NAME}_today_snap_disks.name
        exit_code=$?
     fi
     return $exit_code
@@ -56,15 +58,17 @@ gcd-delete_all_snap ()
         logMsg "INFO" "================================================================================="
         logMsg "INFO" "  Start deleting old backup for $host_name with retention date ${retention_date}"
         logMsg "INFO" "================================================================================="
-        list_of_disks=$( gcloud compute disks list --format='value(name)' --filter="users~'${host_name}$'" )
+        host_zone=$( gcloud compute instances list --format='value(zone)' --filter="name ~ '${host_name}$'" )
+        list_of_disks=$( gcloud compute disks list --format='value(name)' --filter="users ~ '${host_name}$' AND zone:${host_zone}" )
         list_of_snaps=""
         list_of_snaps1=""
         for disk in $list_of_disks
         do
-            list_of_snaps+=$( gcloud compute snapshots list --filter="name:($disk) AND labels.retention_date:'${retention_date}*'" --uri )" "
-            list_of_snaps1+=$( gcloud compute snapshots list --filter="name:($disk) AND labels.retention_date:'${retention_date}*'")", "
+            list_of_snaps+=$( gcloud compute snapshots list --filter="name ~ .*${disk} AND labels.retention_date:'${retention_date}*'" --uri  )" "
+            list_of_snaps1+=$( gcloud compute snapshots list --format='value(name)' --filter="name ~ .*${disk} AND labels.retention_date:'${retention_date}*'"  )", "
         done
-        list_of_snaps1=${list_of_snaps1::-2}
+        #Removing last "," from the end on list_of_snaps1
+        list_of_snaps1=${list_of_snaps1%?}
         logMsg "INFO" "List of snapshots to be deleted. If only comas (,) - no spanpshots to be deleted"
         logMsg "INFO" "$list_of_snaps1"
         for SNAP_NAME in ${list_of_snaps}
@@ -87,9 +91,9 @@ gcd-delete_all_snap ()
               exit_code=$((exit_code+1))
            fi
         done
-        logMsg "INFO" "gcd-backup_all_disk EXIT CODE $exit_code"
+        logMsg "INFO" "gcd-backup_all_disk delete snapshot EXIT CODE $exit_code"
         logMsg "INFO" "================================================================================="
-        logMsg "INFO" "  End creating backup for all disk for $1"
+        logMsg "INFO" "  End delete snaphots for all disk for $1"
         logMsg "INFO" "================================================================================="
     exit $exit_code
     ) 2>&1 | tee -a $LOG_FILE
@@ -106,19 +110,20 @@ gcd-backup_all_disk ()
 {
     exit_code=0
     (
-    pids=""
-        logMsg "INFO" "================================================================================="
-        logMsg "INFO" "  Start creating backup for all disk for $1"
-        logMsg "INFO" "================================================================================="
-        host_zone=$( gcloud compute instances list --format='value(zone)' --filter="name~'${1}$'" )
-        list_of_disks=$( gcloud compute disks list --format='value(name)' --filter="users~'${1}$'" )
+        HOST_NAME=$1
         RETENTION_DAYS=$2
+        pids=""
+        logMsg "INFO" "================================================================================="
+        logMsg "INFO" "  Start creating snapshot for all disk for $1"
+        logMsg "INFO" "================================================================================="
+        host_zone=$( gcloud compute instances list --format='value(zone)' --filter="name ~ '${HOST_NAME}$'" )
+        list_of_disks=$( gcloud compute disks list --format='value(name)' --filter="users ~ '${HOST_NAME}$' AND zone:${host_zone}" )
         for DISK_NAME in ${list_of_disks}
         do
         (
-            gcd_run_gcloud_snap $DISK_NAME $host_zone ${RETENTION_DAYS}
+            gcd_run_gcloud_snap $DISK_NAME $host_zone ${RETENTION_DAYS} ${HOST_NAME}
             exit_code=$?
-        exit $exit_code             
+        exit $exit_code
         ) &
         pids+="$! "
         done
@@ -133,9 +138,9 @@ gcd-backup_all_disk ()
               exit_code=$((exit_code+1))
            fi
         done
-        logMsg "INFO" "gcd-backup_all_disk EXIT CODE $exit_code"
+        logMsg "INFO" "gcd-backup_all_disk snapshot EXIT CODE $exit_code"
         logMsg "INFO" "================================================================================="
-        logMsg "INFO" "  End creating backup for all disk for $1"
+        logMsg "INFO" "  End creating snapshot for all disk for ${HOST_NAME}"
         logMsg "INFO" "================================================================================="
     exit $exit_code
     ) 2>&1 | tee -a $LOG_FILE
@@ -151,14 +156,15 @@ gcd-backup_all_disk ()
 gcd-backup_list_disk ()
 {
     exit_code=0
+    HOST_NAME=$1
     (
         logMsg "INFO" "================================================================================="
         logMsg "INFO" "  Start creating backup of following disks"
         logMsg "INFO" "  $2"
-        logMsg "INFO" "  for $1 host"
+        logMsg "INFO" "  for $HOST_NAME host"
         logMsg "INFO" "================================================================================="
-        ## Get the zone name for instance $1
-        host_zone=$( gcloud compute instances list --format='value(zone)' --filter="name~'${1}$'" )
+        ## Get the zone name for instance
+        host_zone=$( gcloud compute instances list --format='value(zone)' --filter="name='${HOST_NAME}'" )
 
         ## Looping to disk names for creating a snapshot
         local IFS=","
@@ -173,7 +179,7 @@ gcd-backup_list_disk ()
                 RETENTION_DAYS=${DAYS_RETENTION}
             else
                 DISK_NAME=$( echo "${DISK_NAME}" | cut -d ":" -f 1 )
-                logMsg INOF Retention days for ${DISK_NAME} is $RETENTION_DAYS
+                logMsg "INFO" Retention days for ${DISK_NAME} is $RETENTION_DAYS
             fi
             gcd_run_gcloud_snap $DISK_NAME $host_zone $RETENTION_DAYS
             exit_code=$?
@@ -192,10 +198,10 @@ gcd-backup_list_disk ()
               echo "Process $p fail"
           exit_code=$((exit_code+1))
            fi
-        done        
+        done
         logMsg "INFO" "gcd-backup_list_disk EXIT CODE $exit_code"
         logMsg "INFO" "================================================================================="
-        logMsg "INFO" "  End creating backup for disks for $1"
+        logMsg "INFO" "  End creating backup for disks for $HOST_NAME"
         logMsg "INFO" "================================================================================="
         exit $exit_code
     ) 2>&1 | tee -a $LOG_FILE
@@ -203,7 +209,7 @@ gcd-backup_list_disk ()
     if [ $exit_code  -ne 0 ]
     then
     exit_code=1
-    fi  
+    fi
     return $exit_code
 }
 
